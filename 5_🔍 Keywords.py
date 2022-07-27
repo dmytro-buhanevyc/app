6e5a84e31455b9d7ae0232661e1063b42c688866
dima <64311919+dmytro-buhanevyc@users.noreply.github.com>
2022-07-27 17:52:16 +0300

    Add files via upload

---
+++ 5_🔍 Keywords.py
@@ -0,0 +1,24 @@
+
+import streamlit as st
+import streamlit.components.v1 as components
+import base64
+
+
+st.set_page_config(
+        page_title="TCA Data Lab",
+        page_icon=":sunflower:",
+
+    )
+
+st.write("# Keyword search")
+
+
+
+HtmlFile = open("german_keywords.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+print(source_code)
+components.iframe(f"data:text/html;base64,{source_code}", height=800)
+
+progress_bar = st.sidebar.progress(0)
+# We clear elements by calling empty on them.
+progress_bar.empty()
