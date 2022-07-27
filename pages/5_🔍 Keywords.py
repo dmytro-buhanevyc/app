6753eef9acfea4e8b4b741a3d06dac5e5b582814
dima <64311919+dmytro-buhanevyc@users.noreply.github.com>
2022-07-27 17:40:32 +0300

    Add files via upload

--- pages/5_🔍 Keywords.py
+++ pages/5_🔍 Keywords.py
@@ -13,9 +13,9 @@ st.write("# Keyword search")
 
 
 
-HtmlFile = open("german_keywords.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-st.components.v1.html(HtmlFile, width=None, height=None, scrolling=False)
+html = open("german_keywords.html", 'r', encoding='utf-8')
+source_code = html.read() 
+st.components.v1.html(html, width=None, height=None, scrolling=False)
 
 print(source_code)
 components.html(source_code)
